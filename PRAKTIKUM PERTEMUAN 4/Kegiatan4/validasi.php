533faa50c170220c9496f62b899ffbc268c365ce
KrisnaDewa <59174531+krisdewa@users.noreply.github.com>
2021-10-27 20:28:07 +0700

    Update validasi.php

--- PRAKTIKUM PERTEMUAN 4/Kegiatan4/validasi.php
+++ PRAKTIKUM PERTEMUAN 4/Kegiatan4/validasi.php
@@ -9,6 +9,7 @@
         .error {
             color: #FF0000;
         }
+
     </style>
 </head>
 
@@ -18,42 +19,57 @@
     // define variables and set to empty values
     $namaErr = $emailErr = $genderErr = $websiteErr = "";
     $nama = $email = $gender = $comment = $website = "";
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    // Percabangan jika Menerima inputan dari action POST pada FORM inputan
+    if ($_SERVER["REQUEST_METHOD"] == "POST") { // proses jika sudah submit form field
+        // Validasi jika nama kosong maka output "Nama harus diisi"
         if (empty($_POST["nama"])) {
             $namaErr = "Nama harus diisi";
-        } else {
+        } else { //jika nama terisi akan memasukkan data nama ke variabel nama untuk dioutput kan
             $nama = test_input($_POST["nama"]);
         }
+        // Validasi jika email kosong maka output "email harus diisi"
         if (empty($_POST["email"])) {
             $emailErr = "Email harus diisi";
-        } else {
-            $email = test_input($_POST["email"]);
+        } elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)){
             // check if e-mail address is well-formed
-            if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-                $emailErr = "Email tidak sesuai format";
-            }
+            // filter untuk cek apakah email tersebut valid/benar2 email
+            // jika bukan email maka akan output "Email tidak sesuai format"
+            $emailErr = "Email tidak sesuai format";
+        } else { //jika email terisi akan memasukkan data email ke variabel email untuk dioutput kan
+            $email = test_input($_POST["email"]);
         }
+        // Validasi jika website kosong maka output website kosong
         if (empty($_POST["website"])) {
             $website = "";
-        } else {
+        } else { //jika website terisi akan memasukkan data website ke variabel website untuk dioutput kan
             $website = test_input($_POST["website"]);
         }
+        // Validasi jika comment kosong maka output comment kosong
         if (empty($_POST["comment"])) {
             $comment = "";
-        } else {
+        } else { //jika comment terisi akan memasukkan data comment ke variabel comment untuk dioutput kan
             $comment = test_input($_POST["comment"]);
         }
-        if (empty($_POST["gender"])) {
+        // validasi jika data kosong maka output "Gender harus dipilih"
+        if (empty($_POST["gender"])) { 
             $genderErr = "Gender harus dipilih";
-        } else {
+        } else { //jika gender terisi akan memasukkan data gender ke variabel gender untuk dioutput kan
             $gender = test_input($_POST["gender"]);
         }
     }
 
+    // untuk input dan menampilkan data
     function test_input($data) {
+
+        //trim digunakan untuk 'membersihkan' hasil input form dari karakter spasi yang sengaja atau tidak sengaja ditambahkan pengguna.
         $data = trim($data);
+
+        //stripslashes untuk menghapus atau menghilangkan karakter backslashes tanda garis miring terbalik ("\") menggunakan stripslashes() sehingga tidak mengganggu query mysql yang dikirim.
         $data = stripslashes($data);
-        $data = htmlspecialchars($data);
+
+        //htmlspecialchars akan mengkonversi 4 karakter 'khusus' HTML menjadi named entity sehingga tidak akan di 'proses' oleh web browser.
+        $data = htmlspecialchars($data); 
         return $data;
     }
 ?>
@@ -63,29 +79,29 @@
     <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
         <table>
             <tr>
-                <td>Nama:</td>
+                <td>Nama :</td>
                 <td><input type="text" name="nama">
                     <span class="error">* <?php echo $namaErr;?></span>
                 </td>
             </tr>
             <tr>
-                <td>E-mail: </td>
+                <td>E-mail : </td>
                 <td><input type="text" name="email">
                     <span class="error">* <?php echo $emailErr;?></span>
                 </td>
             </tr>
             <tr>
-                <td>Website:</td>
+                <td>Website :</td>
                 <td> <input type="text" name="website">
                     <span class="error"><?php echo $websiteErr;?></span>
                 </td>
             </tr>
             <tr>
-                <td>Komentar:</td>
+                <td>Komentar :</td>
                 <td> <textarea name="comment" rows="5" cols="40"></textarea></td>
             </tr>
             <tr>
-                <td>Gender:</td>
+                <td>Gender :</td>
                 <td>
                     <input type="radio" name="gender" value="L">Laki-Laki
                     <input type="radio" name="gender" value="P">Perempuan
@@ -94,6 +110,7 @@
             </tr>
             <td>
                 <input type="submit" name="submit" value="Submit">
+                
             </td>
         </table>
     </form>
@@ -106,22 +123,18 @@
     echo "<th>Nama</th>";
     echo "<th> ", $nama ," </th>";
     echo "</tr>";
-
     echo "<tr>";
     echo "<th>E-mail</th>";
     echo "<th> ", $email ," </th>";
     echo "</tr>";
-
     echo "<tr>";
     echo "<th>Website</th>";
     echo "<th> ", $website ," </th>";
     echo "</tr>";
-
     echo "<tr>";
     echo "<th>Comment</th>";
     echo "<th> ", $comment ," </th>";
     echo "</tr>";
-
     echo "<tr>";
     echo "<th>Gender</th>";
     echo "<th> ", $gender ," </th>";
