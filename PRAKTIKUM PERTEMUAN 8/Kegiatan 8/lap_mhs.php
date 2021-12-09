e860cc78c566166e06bb23f6f3e8fd86eb4f1526
KrisnaDewa <59174531+krisdewa@users.noreply.github.com>
2021-12-09 23:58:56 +0700

    Up

--- PRAKTIKUM PERTEMUAN 8/Kegiatan 8/lap_mhs.php
+++ PRAKTIKUM PERTEMUAN 8/Kegiatan 8/lap_mhs.php
@@ -5,9 +5,11 @@
     $pdf = new FPDF('l','mm','A5');
     // membuat halaman baru
     $pdf->AddPage();
-    // setting jenis font yang akan digunakan
+    // SETTING jenis font yang akan digunakan
+    //DISINI MENGGUNAKAN FONT ARIAL dengan ukuran 16
     $pdf->SetFont('Arial','B',16);
-    // mencetak string
+
+    // MENCETAK STRING
     $pdf->Cell(190,7,'PROGRAM STUDI TEKNIK INFORMATIKA',0,1,'C');
     $pdf->SetFont('Arial','B',12);
     $pdf->Cell(190,7,'DAFTAR MAHASISWA MATKUL PEMROGRAMAN WEB DINAMIS',0,1,'C');
@@ -21,14 +23,16 @@
     $pdf->Cell(30,6,'TANGGAL LHR',1,1);
     $pdf->SetFont('Arial','',10);
     
-    include 'koneksi.php';
-    $mahasiswa = mysqli_query($con, "SELECT * FROM mahasiswa");
-    while ($row = mysqli_fetch_array($mahasiswa)){
+    include 'koneksi.php'; //include merupakan salah satu fungsi bawaan PHP untuk memanggil file PHP lain dan menyisipkan semua isi file PHP tersebut.
+    $mahasiswa = mysqli_query($con, "SELECT * FROM mahasiswa"); //melakukan query/menampilkan semua data pada table mahasiswa
+
+    //mysql_fetch_array() berfungsi untuk menangkap data dari hasil perintah query dan membentuknya ke dalam array asosiatif dan array numerik.
+    while ($row = mysqli_fetch_array($mahasiswa)){ 
         $pdf->Cell(20,6,$row['nim'],1,0);
         $pdf->Cell(50,6,$row['nama'],1,0);
         $pdf->Cell(25,6,$row['jkel'],1,0);
         $pdf->Cell(50,6,$row['alamat'],1,0);
         $pdf->Cell(30,6,$row['tgllhr'],1,1);
     }
-    $pdf->Output();
+    $pdf->Output(); //berfungsi untuk mengirim PDF ke browser dan memaksa unduhan file dengan nama yang diberikan oleh nama pdf
 ?>
