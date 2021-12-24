5c20b977f376d87bee81ef4869ee7fd6598e3fca
KrisnaDewa <59174531+krisdewa@users.noreply.github.com>
2021-12-24 15:06:12 +0700

    tambah penjelasan

--- PRAKTIKUM PERTEMUAN 10/Kegiatan 10/akses_mhs.php
+++ PRAKTIKUM PERTEMUAN 10/Kegiatan 10/akses_mhs.php
@@ -1,10 +1,18 @@
 <?php
+    // Definisikan url untuk web service
+    // alamat localhost dari getdatamhs.php
     $url = "http://localhost/pemrograman-web-dinamis/PRAKTIKUM%20PERTEMUAN%2010/Kegiatan%2010/getdatamhs.php";
+    //Buat http request ke url web service
     $client = curl_init($url);
+    //Set option data dikembalikan sebagai string
     curl_setopt($client, CURLOPT_RETURNTRANSFER, 1);
+    //simpan hasil ke variabel response
     $response = curl_exec($client);
+    //Dekode dari format json menjadi array
+    //Simpan ke variabel result
     $result = json_decode($response);
-    foreach ($result as $r) {
+    //Tampilkan data yang didapat
+    foreach ($result as $r) { //perulangan untuk mengoutputkan data
         echo "<p>";
         echo "NIM : " . $r -> nim . "<br />";
         echo "Nama : " . $r -> nama . "<br />";
--- PRAKTIKUM PERTEMUAN 10/Kegiatan 10/getdatamhs.php
+++ PRAKTIKUM PERTEMUAN 10/Kegiatan 10/getdatamhs.php
@@ -1,11 +1,19 @@
 <?php
+    //Panggil file untuk koneksi ke database
     require_once "koneksi.php";
+    //Buat query untuk mengambil data dari database
     $sql = "select * from mahasiswa";
+    //Jalankan query
     $query = mysqli_query($con,$sql);
-    while ($row = mysqli_fetch_assoc($query)) {
+    //Masukkan setiap baris data yang didapat kedalam
+    //Array $data
+    while ($row = mysqli_fetch_assoc($query)) { // perulangan untuk memasukkan data ke array
         $data[] = $row;
     }
+    //Deskripsikan jenis isi konten adalah json
     header('content-type: application/json');
+    //Tampilkan data yang didapat dari database dengan format json
     echo json_encode($data);
+    // menutup koneksi
     mysqli_close($con);
 ?>
